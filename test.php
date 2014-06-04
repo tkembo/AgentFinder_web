961cbbbb883ae0337e2f5ee0fbba1085eb1efc17
Tawanda Kembo <tawanda@zimstay.com>
2014-06-04 07:34:08 +0200

    testing another submission

--- test.php
+++ test.php
@@ -3,9 +3,217 @@
 <head>
 <title>Agent Finder</title>
 <script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
-<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDY0kkJiTPVd2U7aTOAwhc9ySH6oHxOIYM&amp;sensor=false"></script>
-<script src="http://maps.google.com/maps/api/js?sensor=false" type="text/javascript"></script>
-<script src="http://maps.google.com/maps/api/js?sensor=false" type="text/javascript"></script>
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=true"></script>
+<!--<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyD2Udawtju2XkRqD1qrut83wvZpa5keH7Q&sensor=false"></script>-->
+<script type="text/javascript">
+$(document).ready(function() {
+
+	//var mapCenter = new google.maps.LatLng(-17.82922, 31.053961); //Google map Coordinates
+	var map;
+
+	map_initialize(); // initialize google map
+
+	//############### Google Map Initialize ##############
+	function map_initialize()
+	{
+			var googleMapOptions = 
+			{ 
+				zoom: 17, //zoom level, 0 = earth view to higher value
+				maxZoom: 18,
+				minZoom: 16,
+				zoomControlOptions: {
+				style: google.maps.ZoomControlStyle.SMALL //zoom control size
+			},
+				scaleControl: true, // enable scale control
+				mapTypeId: google.maps.MapTypeId.ROADMAP // google map type
+			};
+
+		   	map = new google.maps.Map(document.getElementById("google_map"), googleMapOptions);			
+
+			if(navigator.geolocation) {
+			    browserSupportFlag = true;
+			    navigator.geolocation.getCurrentPosition(function(position) {
+			      initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+			      map.setCenter(initialLocation);
+			      var marker = new google.maps.Marker({
+     		 	  position: initialLocation,
+      			  map: map,
+     		 	  title: 'Current Location'
+  			    });
+			    }, function() {
+			      handleNoGeolocation(browserSupportFlag);
+				});
+			}
+			  // Browser doesn't support Geolocation
+			 else {
+				browserSupportFlag = false;
+			    handleNoGeolocation(browserSupportFlag);
+			}
+
+			function handleNoGeolocation(errorFlag) {
+			if (errorFlag == true) {
+				alert("Geolocation service failed.");
+				initialLocation = newyork;
+			} else {
+				alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
+				initialLocation = siberia;
+				}
+				map.setCenter(initialLocation);
+			}
+
+			//Load Markers from the XML File, Check (map_process.php)
+			$.get("map_process.php", function (data) {
+				$(data).find("marker").each(function () {
+					  var name 		= $(this).attr('name');
+					  var address 	= '<p>'+ $(this).attr('address') +'</p>';
+					  var type 		= $(this).attr('type');
+					  var point 	= new google.maps.LatLng(parseFloat($(this).attr('lat')),parseFloat($(this).attr('lng')));
+					  create_marker(point, name, address, false, false, false, "http://agentfinder.co/icons/pin_blue.png");
+				});
+			});	
+
+			//Right Click to Drop a New Marker
+			google.maps.event.addListener(map, 'rightclick', function(event) {
+				//Edit form to be displayed with new marker
+				var EditForm = '<p><div class="marker-edit">'+
+				'<form action="ajax-save.php" method="POST" name="SaveMarker" id="SaveMarker">'+
+				'<label for="pName"><span>Place Name :</span><input type="text" name="pName" class="save-name" placeholder="Enter Title" maxlength="40" /></label>'+
+				'<label for="pDesc"><span>Description :</span><textarea name="pDesc" class="save-desc" placeholder="Enter Address" maxlength="150"></textarea></label>'+
+				'<label for="pType"><span>Type :</span> <select name="pType" class="save-type"><option value="store">Store</option><option value="shopping_mall">Shopping Mall</option>'+
+				'<option value="grocery_or_supermarket">Grocery of Supermarket</option><option value="post_office">Post Office</option></select></label>'+
+				'<label for="agentType"><span>Agent Type :</span> <select name="agentType" class="save-agent_type"><option value="1">Ecocash</option><option value="2">Telecash</option>'+
+				'<option value="3">Onewallet</option></select></label>'+
+				'</form>'+
+				'</div></p><button name="save-marker" class="save-marker">Save Marker Details</button>';
+
+				//Drop a new Marker with our Edit Form
+				create_marker(event.latLng, 'New Marker', EditForm, true, true, true, "http://agentfinder.co/icons/pin_green.png");
+			});
+
+	}
+
+	//############### Create Marker Function ##############
+	function create_marker(MapPos, MapTitle, MapDesc,  InfoOpenDefault, DragAble, Removable, iconPath)
+	{	  	  		  
+
+		//new marker
+		var marker = new google.maps.Marker({
+			position: MapPos,
+			map: map,
+			draggable:DragAble,
+			animation: google.maps.Animation.DROP,
+			title:"Hello World!",
+			icon: iconPath
+		});
+
+		//Content structure of info Window for the Markers
+		var contentString = $('<div class="marker-info-win">'+
+		'<div class="marker-inner-win"><span class="info-content">'+
+		'<h1 class="marker-heading">'+MapTitle+'</h1>'+
+		MapDesc+ 
+		'</span><button name="remove-marker" class="remove-marker" title="Remove Marker">Remove Marker</button>'+
+		'</div></div>');	
+
+
+		//Create an infoWindow
+		var infowindow = new google.maps.InfoWindow();
+		//set the content of infoWindow
+		infowindow.setContent(contentString[0]);
+
+		//Find remove button in infoWindow
+		var removeBtn 	= contentString.find('button.remove-marker')[0];
+		var saveBtn 	= contentString.find('button.save-marker')[0];
+
+		//add click listner to remove marker button
+		google.maps.event.addDomListener(removeBtn, "click", function(event) {
+			remove_marker(marker);
+		});
+
+		if(typeof saveBtn !== 'undefined') //continue only when save button is present
+		{
+			//add click listner to save marker button
+			google.maps.event.addDomListener(saveBtn, "click", function(event) {
+				var mReplace = contentString.find('span.info-content'); //html to be replaced after success
+				var mName = contentString.find('input.save-name')[0].value; //name input field value
+				var mDesc  = contentString.find('textarea.save-desc')[0].value; //description input field value
+				var mType = contentString.find('select.save-type')[0].value; //type of marker
+
+				if(mName =='' || mDesc =='')
+				{
+					alert("Please enter Name and Description!");
+				}else{
+					save_marker(marker, mName, mDesc, mType, mReplace); //call save marker function
+				}
+			});
+		}
+
+		//add click listner to save marker button		 
+		google.maps.event.addListener(marker, 'click', function() {
+				infowindow.open(map,marker); // click on marker opens info window 
+	    });
+
+		if(InfoOpenDefault) //whether info window should be open by default
+		{
+		  infowindow.open(map,marker);
+		}
+	}
+
+	//############### Remove Marker Function ##############
+	function remove_marker(Marker)
+	{
+
+		/* determine whether marker is draggable 
+		new markers are draggable and saved markers are fixed */
+		if(Marker.getDraggable()) 
+		{
+			Marker.setMap(null); //just remove new marker
+		}
+		else
+		{
+			//Remove saved marker from DB and map using jQuery Ajax
+			var mLatLang = Marker.getPosition().toUrlValue(); //get marker position
+			var myData = {del : 'true', latlang : mLatLang}; //post variables
+			$.ajax({
+			  type: "POST",
+			  url: "map_process.php",
+			  data: myData,
+			  success:function(data){
+					Marker.setMap(null); 
+					alert(data);
+				},
+				error:function (xhr, ajaxOptions, thrownError){
+					alert(thrownError); //throw any errors
+				}
+			});
+		}
+
+	}
+
+	//############### Save Marker Function ##############
+	function save_marker(Marker, mName, mAddress, mType, replaceWin)
+	{
+		//Save new marker using jQuery Ajax
+		var mLatLang = Marker.getPosition().toUrlValue(); //get marker position
+		var myData = {name : mName, address : mAddress, latlang : mLatLang, type : mType }; //post variables
+		console.log(replaceWin);		
+		$.ajax({
+		  type: "POST",
+		  url: "map_process.php",
+		  data: myData,
+		  success:function(data){
+				replaceWin.html(data); //replace info window with new html
+				Marker.setDraggable(false); //set marker to fixed
+				Marker.setIcon('http://agentfinder.co/icons/pin_blue.png'); //replace icon
+            },
+            error:function (xhr, ajaxOptions, thrownError){
+                alert(thrownError); //throw any errors
+            }
+		});
+	}
+
+});
+</script>
+
 <style type="text/css">
 h1.heading{padding:0px;margin: 0px 0px 10px 0px;text-align:center;font: 18px Georgia, "Times New Roman", Times, serif;}
 
@@ -32,38 +240,5 @@ button.save-marker, button.remove-marker{border: none;background: rgba(0, 0, 0, 
 <h1 class="heading">Save Agent's location</h1>
 <div align="center">Right Click to Drop a New Marker</div>
 <div id="google_map"></div>
-<script type="text/javascript">
- $(document).ready(function() {
- var map = new google.maps.Map(document.getElementById('google_map'), {
- zoom: 10,
- maxZoom: 18,
- center: new google.maps.LatLng(-17.82922, 31.053961),
- scaleControl: true, // enable scale control
- mapTypeId: google.maps.MapTypeId.ROADMAP // google map type
- });
- var infowindow = new google.maps.InfoWindow();
- var marker, i=0;
- $.get("map_process.php", function (data) {
- $(data).find("marker").each(function () {
- var name = $(this).attr('name');
- var address 	= $(this).attr('address');
- var type 		= $(this).attr('type');
- var lat 	= $(this).attr('lat');
- var log = $(this).attr('lng');
- //create_marker(point, name, address, false, false, false, "http://agentfinder.co/icons/pin_blue.png");
- marker = new google.maps.Marker({
- position: new google.maps.LatLng(lat, log),
- map: map
- });
- google.maps.event.addListener(marker, 'click', (function(marker, i) {
- return function() {
- infowindow.setContent(address);
- infowindow.open(map, marker);
- }
- })(marker, i));
- });
- });
- });
- </script>
 </body>
 </html>
